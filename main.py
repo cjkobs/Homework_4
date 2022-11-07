20cedcc2b1560f0fd0ca4431fa092bd4139d94d3
cjkobs <ckobs@asu.edu>
2022-11-07 14:02:39 -0700

    first draft

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+# here goes nothing
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import math as m
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from sympy import symbols
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def grg():
+    x1, x2 = symbols('x1 x2 x3')
+    xvars = [x1, x2]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    f = x1 ^ 2 + x2 ^ 2 + (x1 + x2) ^ 2
+    h = (x1 ^ 2) / 4 + (x2 ^ 2) / 5 + ((x1 + x2) ^ 2) / 25 - 1
+    alpha = 1.0
+    b = 0.5
+    t = 0.3
+    max_iter = 100
+    eps = 0.001
+
+    x_init = np.array([0, 5 / m.sqrt(6)])
+
+    df = np.array([np.diff(f, xvar) for xvar in xvars])
+    dh = np.array([[np.diff(h, xvar) for xvar in xvars] for h in h])
+    nonbasic_vars = len(xvars) - len(h)
+    opt_sols = []
+
+    for iter
